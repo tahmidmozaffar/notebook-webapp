3a1755b71f92641c01da879ca332b82478e872e2
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-12-23 00:40:11 -0500

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites: () => [
+    {
+      source: '/:api*',
+      destination: `http://localhost:8000/:api*`,
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
