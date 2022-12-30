5ec4cdaa7f01949a49da7fadafcc34b7ab097b47
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2022-12-30 14:17:36 -0500

    title field

--- src/containers/text_editor/index.tsx
+++ src/containers/text_editor/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./texteditor.module.css";
+import commonStyles from "../../../styles/common.module.css";
 import dynamic from "next/dynamic";
 const ReactQuill = dynamic(import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
@@ -14,15 +15,20 @@ const initialValue = [
 const TextEditor = () => {
   const [value, setValue] = useState("");
 
+  console.log(value);
+
   return (
-    <div style={{padding: 10}}>
+    <div className={styles.container}>
+      <div className={commonStyles.flex}>
+        <input placeholder="Title" className={styles.title} />
+        <button className={styles.button}>Save</button>
+      </div>
       <ReactQuill
         className={styles.editor}
         theme="snow"
         value={value}
         onChange={setValue}
       />
-      ;
     </div>
   );
 };
