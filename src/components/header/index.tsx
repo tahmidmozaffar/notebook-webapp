89c5c58219d5a729fb9802fa909fc2ff2581b680
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2023-01-05 03:04:20 -0500

    added user name and logout button on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "./header.module.css";
 
 const Header = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
       <div className={styles.logo_container}>
@@ -15,13 +17,10 @@ const Header = () => {
         <h3 className={styles.logo_title}>Notebook</h3>
       </div>
       <div>
-        <Image
-          src="/logo.png"
-          width={40}
-          height={40}
-          alt="logo"
-          className={styles.profile_icon}
-        />
+        <span className={styles.name}>User name</span>
+        <span className={styles.logout} onClick={() => router.replace("/")}>
+          {"( Logout )"}
+        </span>
       </div>
     </div>
   );
